a99a9a706c9d80ea5348c5b34f0c00d0ad015933
Kody Smith <smithk@versifit.com>
2019-03-07 14:57:51 -0600

    Added character search component

---
+++ src/app/shared/search/search.component.ts
@@ -0,0 +1,93 @@
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+
+import {CharacterSearchResult, CharacterSearchResultRow, Pagination} from '@xivapi/angular-client';
+import {Subject, of} from 'rxjs';
+import {catchError, debounceTime, distinctUntilChanged, finalize, map, switchMap} from 'rxjs/operators';
+
+import {SearchService} from './search.service';
+import {Character} from '../models/character';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit, OnDestroy {
+  @Output() selected: EventEmitter<Character> = new EventEmitter();
+  @Input() clearOnSelect = false;
+
+  selectedCharacter: CharacterSearchResultRow;
+  searchCharacterInput$ = new Subject<string>();
+  lastInput: string;
+  searchIsExecuting = false;
+
+  characters: CharacterSearchResultRow[] = [];
+  pagination: Pagination;
+  constructor(private searchService: SearchService) { }
+
+  ngOnInit() {
+    // Setup auto search on change of search input
+    this.searchCharacterInput$.pipe(
+      debounceTime(1000), // 1 second
+      map(term => term ? term.toLocaleLowerCase() : ''),
+      distinctUntilChanged(),
+      switchMap(input => {
+        // Reset pagination between searches, and save the input for next page requests
+        this.pagination = undefined;
+        this.lastInput = input;
+        // Return empty list if no search term or < 3 characters, otherwise execute character search
+        if (!input || input.length < 3) {
+          return of([]);
+        } else {
+          return this.search(input);
+        }
+      })
+    ).subscribe( characters => {
+      this.characters = characters;
+    });
+  }
+
+  /**
+   * Executed when the end of the dropdown is reached
+   */
+  onScrollToEnd() {
+    // If we have pagination and a next page, retrieve the next page of characters and append it on
+    if (this.pagination && this.pagination.PageNext) {
+      this.search(this.lastInput, this.pagination.PageNext as number).subscribe( characters => {
+        this.characters = this.characters.concat(characters);
+      });
+    }
+  }
+
+  /**
+   * Executes a search, saves the pagination globally, and returns the characters.
+   * @param searchTerm - The search term.
+   * @param page - The page of matches to return.
+   */
+  search(searchTerm: string, page?: number) {
+    this.searchIsExecuting = true;
+    return this.searchService.searchCharacter(searchTerm, undefined, page).pipe(
+      map((result: CharacterSearchResult) => {
+        // Save pagination, return list of characters for first page
+        this.pagination = result.Pagination;
+        return result.Results;
+      }),
+      finalize(() => this.searchIsExecuting = false),
+      catchError(() => of([])) // empty list on error
+    );
+  }
+  onChange() {
+    // Output a new character, then wipe the ng select
+    const character: Character = !this.selectedCharacter ? null : {
+      id: this.selectedCharacter.ID,
+      name: this.selectedCharacter.Name,
+      server: this.selectedCharacter.Server
+  };
+    this.selected.emit(character);
+  }
+  ngOnDestroy(): void {
+    if (this.searchCharacterInput$) {
+      this.searchCharacterInput$.unsubscribe();
+    }
+  }
+}
---
+++ src/app/shared/search/search.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+import {XivapiService} from '@xivapi/angular-client';
+import {catchError} from 'rxjs/operators';
+
+import {PNotifyService} from '../notifications/pnotify-service.service';
+import {XivapiService2} from '../xivapi/xiv-api-service2.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+
+  constructor(private xivAPI: XivapiService, private xivAPI2: XivapiService2, private notifyService: PNotifyService) { }
+  searchCharacter(searchTerm: string, server?: string, page?: number) {
+    return this.xivAPI.searchCharacter(searchTerm, server, page).pipe(
+      catchError(err => this.notifyService.error({text: err.Message}))
+    );
+  }
+  getServers() {
+    return this.xivAPI.getServerList();
+  }
+  getServersDC() {
+    return this.xivAPI2.getServerListByDC();
+  }
+}
