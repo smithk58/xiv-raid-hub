a3e103bc29e091c8b2cc1b566d0e7cf42b132403
Kody Smith <smithk@versifit.com>
2019-03-07 15:01:44 -0600

    Added dummy home, analyze, not found, and settings pages

---
+++ src/app/pages/analyze/analyze.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-analyze',
+  templateUrl: './analyze.component.html',
+  styleUrls: ['./analyze.component.css']
+})
+export class AnalyzeComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
