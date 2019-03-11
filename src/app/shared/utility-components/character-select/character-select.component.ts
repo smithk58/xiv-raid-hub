60a5bcbf394f8aa8d61dce72bc081020646e22a9
Kody Smith <smithk@versifit.com>
2019-03-10 21:11:08 -0500

    Added character and group select components, added alt tags in character search component

---
+++ src/app/shared/utility-components/character-select/character-select.component.ts
@@ -0,0 +1,21 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+import {Character} from 'src/app/shared/api/models/character';
+import {ClassToRole} from 'src/app/shared/Utils';
+
+@Component({
+  selector: 'app-character-select',
+  templateUrl: './character-select.component.html',
+  styleUrls: ['./character-select.component.css']
+})
+export class CharacterSelectComponent {
+  @Input() selectedValue: number;
+  classToRole = ClassToRole;
+  @Input() characters: Character[] = [];
+  @Input() labelForId: string;
+  @Output() selected: EventEmitter<Character> = new EventEmitter();
+  constructor() { }
+  onChange(character: Character) {
+    this.selected.emit(character);
+  }
+}
---
+++ src/app/shared/utility-components/group-select/group-select.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-group-select',
+  templateUrl: './group-select.component.html',
+  styleUrls: ['./group-select.component.css']
+})
+export class GroupSelectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
