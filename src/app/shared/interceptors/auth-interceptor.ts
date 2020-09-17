0bfeb49695112b89a4300fdd9b905b1ad8fd4772
Kody Smith <smithk@versifit.com>
2020-09-16 21:32:04 -0500

    401s now refresh session and redirect to home

---
+++ src/app/shared/interceptors/auth-interceptor.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpRequest, HttpInterceptor, HttpHandler, HttpErrorResponse } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { UserService } from 'src/app/shared/api/xiv-raid-hub/user.service';
+import { PNotifyService } from 'src/app/shared/notifications/pnotify-service.service';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private userService: UserService, private notify: PNotifyService, private router: Router) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // Catch any errors and simplify them
+    return next.handle(request).pipe(
+      catchError(err => this.handleAuthenticationError(err, request, next))
+    );
+  }
+
+  /**
+   * Checks an http error for a 401 on internal http calls and kills the front end session if found.
+   * @param response
+   * @param request
+   * @param next
+   */
+  handleAuthenticationError(response: HttpErrorResponse, request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (response.status === 401) {
+      this.notify.error({text: 'Your session expired, please log in again.'});
+      this.userService.refreshSession();
+      this.router.navigate(['/home']);
+    }
+    return throwError(response);
+  }
+}
--- src/app/shared/interceptors/index.ts
+++ src/app/shared/interceptors/index.ts
@@ -3,11 +3,13 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ErrorInterceptor } from 'src/app/shared/interceptors/error-interceptor';
 import { CachingInterceptor } from 'src/app/shared/interceptors/caching-interceptor';
 import { ApiUrlInterceptor } from 'src/app/shared/interceptors/api-url-interceptor';
+import { AuthInterceptor } from 'src/app/shared/interceptors/auth-interceptor';
 
 /** Http interceptor providers in outside-in order */
 
 export const httpInterceptorProviders = [
   { provide: HTTP_INTERCEPTORS, useClass: ApiUrlInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: CachingInterceptor, multi: true }
 ];
