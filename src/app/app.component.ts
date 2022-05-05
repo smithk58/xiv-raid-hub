eb3fa3f106ffb230f8c9c8dda17ad2212da7e275
Kody Smith <smithk@versifit.com>
2022-05-04 19:20:43 -0500

    Moved initial theme application to index.html header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,20 +13,9 @@ export class AppComponent implements OnInit {
     tooltipConfig.openDelay = 1000;
     popoverConfig.openDelay = 1000;
     popoverConfig.triggers = 'mouseenter:mouseleave';
-    this.appendTheme();
   }
 
   ngOnInit() {
     this.userService.refreshSession();
-  }
-  appendTheme() {
-    // TODO Should be controlled server side to prevent flicker, but too lazy atm
-    // Load theme
-    const theme = (localStorage.getItem('theme') || 'default') + '-theme.css';
-    const lazyStyleElement = document.createElement('link');
-    lazyStyleElement.id = 'bootstrap-theme';
-    lazyStyleElement.rel = 'stylesheet';
-    lazyStyleElement.href = theme;
-    document.head.appendChild(lazyStyleElement);
   }
 }
