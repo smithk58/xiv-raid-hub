e5c95a1ad09ae61bbf17c77639d49f7b10d64f25
Kody Smith <smithk@versifit.com>
2023-11-19 01:36:39 -0600

    Moved some scheduler modal functions to a service

--- src/app/pages/configuration/modals/scheduler/scheduler.component.ts
+++ src/app/pages/configuration/modals/scheduler/scheduler.component.ts
@@ -16,6 +16,7 @@ import { UserService } from 'src/app/shared/api/xiv-raid-hub/user.service';
 import { DaysOfWeek } from 'src/app/shared/DaysUtils';
 import { RaidGroupService } from 'src/app/shared/api/xiv-raid-hub/raid-group.service';
 import { PNotifyService } from 'src/app/shared/notifications/pnotify-service.service';
+import { SchedulerService } from 'src/app/pages/configuration/modals/scheduler/scheduler.service';
 
 @Component({
   selector: 'app-scheduler',
@@ -33,7 +34,7 @@ export class SchedulerComponent implements OnInit {
   timezone: string;
   isLoaded = false;
   constructor(private modal: NgbActiveModal, private formBuilder: FormBuilder, private userService: UserService,
-              private raidGroupService: RaidGroupService, private notify: PNotifyService
+              private raidGroupService: RaidGroupService, private notify: PNotifyService, private schedulerService: SchedulerService
   ) { }
 
   ngOnInit(): void {
@@ -45,6 +46,7 @@ export class SchedulerComponent implements OnInit {
     this.scheduleForm = this.formBuilder.group({
       weeklyRaidTimes: this.weeklyRaidTimes
     });
+    // Add any existing weekly raid times to the form
     this.raidGroupService.getRaidGroupsRaidTimes(this.raidGroupId).pipe(
       finalize(() => {this.isLoaded = true; })
     ).subscribe((raidTimes) => {
@@ -72,18 +74,9 @@ export class SchedulerComponent implements OnInit {
     let initialDaysInWeek: boolean[];
     let initialTime: NgbTimeStruct;
     if (raidTime) {
-      // Convert the time to the users local hours/minutes
-      const time = new Date();
-      time.setUTCHours(raidTime.utcHour, raidTime.utcMinute, 0);
-      initialTime = {hour: time.getHours(), minute: time.getMinutes(), second: 0};
-      // Convert the selected days from UTC to local users days
-      const utcDaysInWeek = daysInWeekMaskToBools(raidTime.utcWeekMask);
-      const shiftDir = this.getDayShiftDirection(time, 'local');
-      if (shiftDir) {
-        initialDaysInWeek = this.shiftDayBooleanArray(utcDaysInWeek, shiftDir);
-      } else {
-        initialDaysInWeek = utcDaysInWeek; // no shifting needed, the days already match up to the local users
-      }
+      const localTime = this.schedulerService.weeklyRaidTimeToLocal(raidTime);
+      initialDaysInWeek = localTime.localDaysInWeek;
+      initialTime = localTime.localTime;
     } else {
       initialTime = {hour: 12, minute: 0, second: 0}; // 12pm default
     }
@@ -118,9 +111,9 @@ export class SchedulerComponent implements OnInit {
         startTime.setSeconds(0);
         // If the UTC day is different than the users day, then adjust the days array to match UTC days before saving
         let daysOfWeekUtc = raidTime.daysOfWeek;
-        const shiftDir = this.getDayShiftDirection(startTime, 'utc');
+        const shiftDir = this.schedulerService.getDayShiftDirection(startTime, 'utc');
         if (shiftDir) {
-          daysOfWeekUtc = this.shiftDayBooleanArray(raidTime.daysOfWeek, shiftDir);
+          daysOfWeekUtc = this.schedulerService.shiftDayBooleanArray(raidTime.daysOfWeek, shiftDir);
         }
         raidTimes.push({
           raidGroupId: this.raidGroupId,
@@ -149,47 +142,6 @@ export class SchedulerComponent implements OnInit {
       }
     }
     return {required: true};
-  }
-
-  /**
-   * Calculates whether you need to shift the days left or right in the days boolean array, depending on if you want the array to be the
-   * users local days or UTC days.
-   * @param date - A day representing a particular raid time (the date doesn't matter, only the time).
-   * @param target - Whether you want to shift the days to local days or UTC.
-   */
-  getDayShiftDirection(date: Date, target: 'local' | 'utc') {
-    const dayOfWeek = date.getDay();
-    const dayOfWeekUTC = date.getUTCDay();
-    if (dayOfWeekUTC === dayOfWeek) {
-      return undefined;
-    }
-    // Positive timezone offset means local time is earlier than UTC (i.e. local + offset = utc)
-    const localTimeIsEarlier = date.getTimezoneOffset() > 0;
-    if (target === 'utc') {
-      // if local time is earlier, then UTC day must be equal to or greater than it, so we want to shift right
-      // i.e. if local day is monday, utc must be monday or tuesday if local time is earlier
-      return localTimeIsEarlier ? 'right' : 'left';
-    }
-    return localTimeIsEarlier ? 'left' : 'right';
-  }
-  /**
-   * Shifts the trues in the boolean array left or right to handle local day -> UTC day or vice versa.
-   * @param values - An array of booleans.
-   * @param shiftDirection - true values will be shifted left/right based on this.
-   */
-  shiftDayBooleanArray(values: boolean[], shiftDirection: 'left' | 'right') {
-    const nValues = [];
-    for (let i = 0; i < values.length; i++) {
-      let tIdx = shiftDirection === 'left' ? (i - 1) : (i + 1);
-      // wrap the index if it under/overflows the array bounds
-      if (tIdx < 0) {
-        tIdx = values.length - 1;
-      } else if (tIdx === values.length) {
-        tIdx = 0;
-      }
-      nValues[tIdx] = values[i];
-    }
-    return nValues;
   }
   // convenience getter for easy access to form fields
   get f() { return this.scheduleForm.controls; }
---
+++ src/app/pages/configuration/modals/scheduler/scheduler.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { daysInWeekMaskToBools, WeeklyRaidTime } from 'src/app/pages/configuration/modals/scheduler/WeeklyRaidTime';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SchedulerService {
+  constructor() { }
+
+  /**
+   * Returns the local time/week mask for the provided weekly raid time, which is in UTC.
+   * @param raidTime -
+   */
+  weeklyRaidTimeToLocal(raidTime: WeeklyRaidTime) {
+    let localTime, localDaysInWeek;
+    // Convert the time to the users local hours/minutes
+    const time = new Date();
+    time.setUTCHours(raidTime.utcHour, raidTime.utcMinute, 0);
+    localTime = {hour: time.getHours(), minute: time.getMinutes(), second: 0};
+    // Convert the selected days from UTC to local users days
+    const utcDaysInWeek = daysInWeekMaskToBools(raidTime.utcWeekMask);
+    const shiftDir = this.getDayShiftDirection(time, 'local');
+    if (shiftDir) {
+      localDaysInWeek = this.shiftDayBooleanArray(utcDaysInWeek, shiftDir);
+    } else {
+      localDaysInWeek = utcDaysInWeek; // no shifting needed, the days already match up to the local users
+    }
+    return {localTime, localDaysInWeek};
+  }
+  /**
+   * Calculates whether you need to shift the days left or right in the days boolean array, depending on if you want the array to be the
+   * users local days or UTC days.
+   * @param date - A day representing a particular raid time (the date doesn't matter, only the time).
+   * @param target - Whether you want to shift the days to local days or UTC.
+   */
+  getDayShiftDirection(date: Date, target: 'local' | 'utc') {
+    const dayOfWeek = date.getDay();
+    const dayOfWeekUTC = date.getUTCDay();
+    if (dayOfWeekUTC === dayOfWeek) {
+      return undefined;
+    }
+    // Positive timezone offset means local time is earlier than UTC (i.e. local + offset = utc)
+    const localTimeIsEarlier = date.getTimezoneOffset() > 0;
+    if (target === 'utc') {
+      // if local time is earlier, then UTC day must be equal to or greater than it, so we want to shift right
+      // i.e. if local day is monday, utc must be monday or tuesday if local time is earlier
+      return localTimeIsEarlier ? 'right' : 'left';
+    }
+    return localTimeIsEarlier ? 'left' : 'right';
+  }
+  /**
+   * Shifts the trues in the boolean array left or right to handle local day -> UTC day or vice versa.
+   * @param values - An array of booleans.
+   * @param shiftDirection - true values will be shifted left/right based on this.
+   */
+  shiftDayBooleanArray(values: boolean[], shiftDirection: 'left' | 'right') {
+    const nValues = [];
+    for (let i = 0; i < values.length; i++) {
+      let tIdx = shiftDirection === 'left' ? (i - 1) : (i + 1);
+      // wrap the index if it under/overflows the array bounds
+      if (tIdx < 0) {
+        tIdx = values.length - 1;
+      } else if (tIdx === values.length) {
+        tIdx = 0;
+      }
+      nValues[tIdx] = values[i];
+    }
+    return nValues;
+  }
+}
