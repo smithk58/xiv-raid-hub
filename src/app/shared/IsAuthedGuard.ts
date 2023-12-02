11da2ed66fb3631e91f01c2f4a949568483f1928
Kody Smith <smithk@versifit.com>
2023-12-02 00:13:21 -0600

    Upgrade angular 15 -> 16

--- src/app/shared/IsAuthedGuard.ts
+++ src/app/shared/IsAuthedGuard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, Router } from '@angular/router';
 
 import { map } from 'rxjs/operators';
 import { PNotifyService } from 'src/app/shared/notifications/pnotify-service.service';
 import { UserService } from 'src/app/shared/api/xiv-raid-hub/user.service';
 
 @Injectable()
-export class IsAuthedGuard implements CanActivate {
+export class IsAuthedGuard  {
   constructor(private router: Router, private userService: UserService, private pNotify: PNotifyService) {}
 
   canActivate(route: ActivatedRouteSnapshot) {
